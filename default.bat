b84c1dd34c00c15d9c18760cc41df127f8e6fb0f
Noble Chukwukere <NOBLEOSINACHI98@GMAIL.COM>
2023-02-24 23:24:31 +0100

    Created 20230224232428

--- default.bat
+++ default.bat
@@ -7,7 +7,7 @@ for /f "delims=" %%a in ('wmic os get LocalDateTime ^| findstr ^^[0-9]') do set 
 set "timestamp=%timestamp:~0,14%"
 
 REM Create the folder with the timestamp as its name
-mkdir %timestamp%
+mkdir models\%timestamp%
 
 REM Create the timestamp.txt file inside the folder
 echo %timestamp% > models\%timestamp%\%timestamp%.txt
@@ -27,17 +27,12 @@ REM Get the current date and time in a format that can be used as a folder name
 for /f "delims=" %%a in ('wmic os get LocalDateTime ^| findstr ^^[0-9]') do set "timestamp=%%a"
 set "timestamp=%timestamp:~0,14%"
 
-REM Create the folder with the timestamp as its name
-mkdir %timestamp%
-
-REM Create the timestamp.txt file inside the folder
-echo %timestamp% > %timestamp%\%timestamp%.txt
 
 git pull origin main
 
 git add .
 
-git commit -m "Created %timestamp% %%timestamp%% !timestamp!"
+git commit -m "Created %timestamp%"
 
 git push origin
 
